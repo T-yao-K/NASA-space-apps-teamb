edfe355dd3f620532e8acfc53c7f4139cb54c8e2
LAPTOP-IUTUOCU5\hidea <kabenoonn@gmail.com>
2024-10-05 15:48:30 +0900

    音楽をNew Tabの時にかけようとしてうまくいきません助けて

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
 let audio = new Audio('audio.mp3');
 audio.loop = true;
-audio.play();
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url === chrome.runtime.getURL('index.html')) {
+      audio.play();
+    }
+  });
+  
+  chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    chrome.tabs.query({url: chrome.runtime.getURL('index.html')}, (tabs) => {
+      if (tabs.length === 0) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    });
+  });
