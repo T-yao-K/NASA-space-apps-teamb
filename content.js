022d5866fe1a73eab543818d1fff2054aab2e19a
LAPTOP-IUTUOCU5\hidea <kabenoonn@gmail.com>
2024-10-05 12:46:11 +0900

    README更新とデバッグ途中

--- content.js
+++ content.js
@@ -1,9 +1,19 @@
+console.log('Content script loaded');
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === "change_background") {
-      if (request.color === "reset") {
-        document.body.style.backgroundColor = "";
-      } else {
-        document.body.style.backgroundColor = request.color;
-      }
+  console.log('Message received in content script:', request);
+
+  if (request.action === "change_background") {
+    console.log('Changing background color to:', request.color);
+
+    if (request.color === "reset") {
+      document.body.style.backgroundColor = "";
+      console.log('Background color reset');
+    } else {
+      document.body.style.backgroundColor = request.color;
+      console.log('Background color changed to:', request.color);
     }
-  });
+
+    sendResponse({status: "Color change attempt completed"});
+  }
+});
--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var buttons = document.getElementsByTagName('button');
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', function() {
-        var color = this.id;
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {action: "change_background", color: color});
+  console.log('Popup DOM fully loaded and parsed');
+
+  var buttons = document.getElementsByTagName('button');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      var color = this.id;
+      console.log('Button clicked: ' + color);
+
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log('Sending message to content script');
+        chrome.tabs.sendMessage(tabs[0].id, {action: "change_background", color: color}, function(response) {
+          console.log('Response from content script:', response);
         });
       });
-    }
-  });
+    });
+  }
+});
